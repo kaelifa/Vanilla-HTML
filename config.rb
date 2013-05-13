96f09514a9525a8b294c0e7ff0801b84770bceb6
Kristina Auckland <kristina.auckland@bss.org>
2013-05-13 16:24:54 +0100

    lining up stuff

--- config.rb
+++ config.rb
@@ -3,26 +3,26 @@ require "susy"
 require 'modular-scale'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "css-src"
-images_dir = "template-assets"
+http_path       = "/"
+css_dir         = "css"
+sass_dir        = "css-src"
+images_dir      = "template-assets"
 javascripts_dir = "js"
-fonts_dir = "fonts"
+fonts_dir       = "fonts"
 
-# output_style = :compressed
-# output_style = :compact
-# output_style = :nested
-output_style = :expanded
-environment = :production
-# environment = :development
+# output_style  = :compressed
+# output_style  = :compact
+# output_style  = :nested
+output_style    = :expanded
+environment     = :production
+# environment   = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-color_output = false
+color_output    = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
